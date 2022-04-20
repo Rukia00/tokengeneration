3cf6091557271f4d490b274ba2198397b76115d9
Rukia00 <95847760+Rukia00@users.noreply.github.com>
2022-04-20 11:43:44 +0900

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 import random
 
 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-
+#生成する数
 for i in range(100):
     first = ''.join((random.choice(chars) for i in range(24)))
     second =''.join((random.choice(chars) for i in range(6)))
